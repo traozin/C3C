9c9020572d012cc0ef54c2ea595d132ea85acb18
Antônio Neto <acm.neto1999@gmail.com>
2022-08-27 17:03:25 -0300

    Q3 Implementada

---
+++ Q3 - SistemasDeNota/index.js
@@ -0,0 +1,23 @@
+function arredonda(nota) {
+    if (nota < 38) {
+        return nota;
+    } else {
+        return Math.ceil(nota / 5) * 5;
+    }
+}
+
+function classificaAluno(nota) {
+    notaArredondada = arredonda(nota);
+    if (notaArredondada < 40) {
+        return "Reprovado com nota: " + notaArredondada;
+    } else {
+        return "Aprovado com nota " + ((notaArredondada - nota) < 3 ? notaArredondada : nota);
+    }
+}
+
+
+var readlineSync = require('readline-sync');
+
+console.log('Informe a nota do estudante!');
+
+console.log(classificaAluno(Number(readlineSync.question('Nota: '))));
